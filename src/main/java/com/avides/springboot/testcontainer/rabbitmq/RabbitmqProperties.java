6d93adcdd7fe7b87ce5c348ebc008fcc4f54c4f5
Henrik Adamski <hadamski@avides.com>
2019-11-11 17:19:23 +0100

    LIBS-562 - Update rabbitmq docker image (#15)

--- src/main/java/com/avides/springboot/testcontainer/rabbitmq/RabbitmqProperties.java
+++ src/main/java/com/avides/springboot/testcontainer/rabbitmq/RabbitmqProperties.java
@@ -26,6 +26,6 @@ public class RabbitmqProperties extends AbstractEmbeddedContainerProperties
 
     public RabbitmqProperties()
     {
-        setDockerImage("rabbitmq:3.7.8-alpine");
+        setDockerImage("rabbitmq:3.8.1-alpine");
     }
 }
